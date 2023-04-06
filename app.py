e3c3e7c1f53a7d3a40f8d829a07eaff0c35edf72
himanshu <himanshupandey1036@gmail.com>
2023-04-06 19:43:36 +0530

    FMI Predictor

--- app.py
+++ app.py
@@ -1,10 +1,41 @@
-from flask import Flask
+from flask import Flask,render_template,jsonify,request
+import numpy as np
+import pandas as pd
+import pickle
 
-app = Flask(__name__)
+application = Flask(__name__)
+app = application
+app.config['TEMPLATES_AUTO_RELOAD']=True
+
+# Importing the model
+scaler_model=pickle.load(open("model/scaler.pkl","rb"))
+rigid_model=pickle.load(open("model/rigid.pkl","rb"))
 
 @app.route("/")
-def hello_world():
-    return "<h1>Hello, World!</h1>"
+def index():
+    return render_template('index.html',title='Form',FMI="")
+
+@app.route("/predict",methods=['GET','POST'])
+def predict():
+    if request.method=='POST':
+        Temprature=request.form['Temprature']
+        RH=request.form['RH']
+        Ws=request.form['Ws']
+        Rain=request.form['Rain']
+        FFMC=request.form['FFMC']
+        DMC=request.form['DMC']
+        ISI=request.form['ISI']
+        Classes=request.form['Classes']
+        Region=request.form['Region']
+
+        scaled_data=scaler_model.transform([[Temprature,RH,Ws,Rain,FFMC,DMC,ISI,Classes,Region]])
+        output=rigid_model.predict(scaled_data)
+
+        FMI="The Value of FMI will be "+str(output[0])
+        return render_template("index.html",title='Form',FMI=FMI)
+    else:
+        return render_template("index.html",title='Form',FMI="")
+    
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0",port=5000,debug=True)
